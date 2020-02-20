cec028b93795e3b1e3c8ba94289657a6e712fa94
nikogilian <57024652+nikogilian@users.noreply.github.com>
2020-02-19 17:10:16 -0800

    Update mailer.php

--- PHP contact form/mailer.php
+++ PHP contact form/mailer.php
@@ -15,7 +15,7 @@ $name_field = $_POST['name'];
 $email_field = $_POST['email'];
 $message = $_POST['message'];
 
-$body = "From: $name_field\n E-Mail: $email_field\n Phone: $phone_field\n Message:\n $message";
+$body = "From: $name_field\n E-Mail: $email_field\n Message:\n $message";
 
 echo "Data has been submitted to $to!";
 mail($to, $subject, $body);
